c7f7254edcefb87e68caa9d4656a6bd9848d18a9
Fabio Baltieri <fabio.baltieri@gmail.com>
2013-04-19 20:08:39 +0200

    bl: update license information

--- bootloader/usbdfu.c
+++ bootloader/usbdfu.c
@@ -1,7 +1,8 @@
 /*
- * This file is part of the libopencm3 project.
+ * Based on usbdfu.c from libopencm3 project.
  *
  * Copyright (C) 2010 Gareth McMullin <gareth@blacksphere.co.nz>
+ * Copyright (C) 2013 Fabio Baltieri <fabio.baltieri@gmail.com>
  *
  * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
