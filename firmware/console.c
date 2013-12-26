8efa349786a83648dadb0faf74acad6d97bf9870
Fabio Baltieri <fabio.baltieri@gmail.com>
2013-12-26 16:19:49 +0000

    fw: add net information console command

--- firmware/console.c
+++ firmware/console.c
@@ -4,6 +4,10 @@
 #include <shell.h>
 #include <string.h>
 
+#include <lwip/netif.h>
+#include <lwip/dhcp.h>
+#include <lwip/stats.h>
+
 #include "console.h"
 #include "data.h"
 
@@ -55,10 +59,59 @@ static void cmd_data_dump(BaseSequentialStream *chp, int argc, char *argv[])
 	data_dump(chp);
 }
 
+#define pflag(c, f, b, s) do {if (f & b) chprintf(c, " " s);} while (0)
+static void cmd_net(BaseSequentialStream *chp, int argc, char *argv[])
+{
+	struct netif *nf = netif_default;
+	struct dhcp *dhcp = nf->dhcp;
+	int i;
+
+	chprintf(chp, "Interface %d: %c%c\r\n",
+			nf->num, nf->name[0], nf->name[1]);
+	chprintf(chp, "  ip_addr: %d.%d.%d.%d\r\n",
+			ip4_addr1(&nf->ip_addr.addr),
+			ip4_addr2(&nf->ip_addr.addr),
+			ip4_addr3(&nf->ip_addr.addr),
+			ip4_addr4(&nf->ip_addr.addr));
+	chprintf(chp, "  netmask: %d.%d.%d.%d\r\n",
+			ip4_addr1(&nf->netmask.addr),
+			ip4_addr2(&nf->netmask.addr),
+			ip4_addr3(&nf->netmask.addr),
+			ip4_addr4(&nf->netmask.addr));
+	chprintf(chp, "  gw: %d.%d.%d.%d\r\n",
+			ip4_addr1(&nf->gw.addr),
+			ip4_addr2(&nf->gw.addr),
+			ip4_addr3(&nf->gw.addr),
+			ip4_addr4(&nf->gw.addr));
+	chprintf(chp, "  hostname: %s\r\n", nf->hostname);
+	chprintf(chp, "  mtu: %d\r\n", nf->mtu);
+	chprintf(chp, "  hwaddr: ");
+	for (i = 0; i < nf->hwaddr_len; i++)
+		chprintf(chp, "%02x", nf->hwaddr[i]);
+	chprintf(chp, "\r\n");
+	chprintf(chp, "  flags (%x):", nf->flags);
+	pflag(chp, nf->flags, NETIF_FLAG_UP, "up");
+	pflag(chp, nf->flags, NETIF_FLAG_BROADCAST, "broadcast");
+	pflag(chp, nf->flags, NETIF_FLAG_POINTTOPOINT, "pointtopoint");
+	pflag(chp, nf->flags, NETIF_FLAG_DHCP, "dhcp");
+	pflag(chp, nf->flags, NETIF_FLAG_LINK_UP, "link_up");
+	pflag(chp, nf->flags, NETIF_FLAG_ETHARP, "etharp");
+	pflag(chp, nf->flags, NETIF_FLAG_ETHERNET, "ethernet");
+	pflag(chp, nf->flags, NETIF_FLAG_IGMP, "igmp");
+	chprintf(chp, "\r\n");
+
+	chprintf(chp, "DHCP:\r\n");
+	chprintf(chp, "  state: %d\r\n", dhcp->state);
+	chprintf(chp, "  tries: %d\r\n", dhcp->tries);
+
+	nf = nf->next;
+}
+
 static const ShellCommand commands[] = {
 	{"mem", cmd_mem},
 	{"ps", cmd_ps},
 	{"dd", cmd_data_dump},
+	{"net", cmd_net},
 	{}
 };
 
